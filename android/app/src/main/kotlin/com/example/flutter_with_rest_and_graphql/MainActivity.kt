37d54ef97e81219baa8511b457e13e4a4d2d2ddd
Arnal Shoorukov <arnal.shoorukov@gmail.com>
2020-01-26 23:51:29 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_with_rest_and_graphql/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flutter_with_rest_and_graphql
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

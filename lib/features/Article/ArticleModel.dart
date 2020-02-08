35c9708499a675849fcbae3754993a74ac480ca4
Arnal Shoorukov <arnal.shoorukov@gmail.com>
2020-02-09 00:47:39 +0200

    Grapghl

--- lib/features/Article/ArticleModel.dart
+++ lib/features/Article/ArticleModel.dart
@@ -1,18 +1,14 @@
-import 'dart:convert';
-
-class ArticleModel{
-  
+class ArticleModel {
   final String title;
-  final String description; 
-  final String avator; 
+  final String description;
+  final String avator;
   final int reactionCount;
   final List<String> tags;
 
-  ArticleModel.fromJSON(Map<String, dynamic> json):
-  
-  title = json['title'],
-  description = json['description'],
-  avator = json['user']['profileImage'], 
-  reactionCount = json['positiveReactionsCount'], 
-  tags = json['tags'] ?? [];
+  ArticleModel.fromJSON(Map<String, dynamic> json)
+      : title = json['title'],
+        description = json['description'],
+        avator = json['user']['profileImage'],
+        reactionCount = json['positiveReactionsCount'],
+        tags = json['tags'] ?? [];
 }
